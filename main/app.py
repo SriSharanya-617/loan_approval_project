8d5c268a0a0e959043e1d04d1bc30673b36630d1
SriSharanya-617 <sharanyavinjamuri9917@gmail.com>
2026-01-22 14:21:20 +0530

    ok

--- main/app.py
+++ main/app.py
@@ -45,7 +45,8 @@ st.write("This system uses **Support Vector Machines (SVM)** to predict loan app
 # ---------------- Load Dataset ----------------
 @st.cache_data
 def load_data():
-    df = pd.read_csv("loan.csv")
+    df = pd.read_csv("main/loan.csv")
+
     return df
 
 
